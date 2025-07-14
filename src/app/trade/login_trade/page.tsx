90f580d2464e274c4e16053c31026d5277bc78e6
Omar Ahmed <Omar-Ahmed-OHM@users.noreply.github.com>
2025-07-14 15:22:01 +0300

    test production mode

--- src/app/trade/login_trade/page.tsx
+++ src/app/trade/login_trade/page.tsx
@@ -41,12 +41,7 @@ const handleSubmit = async (e: React.FormEvent) => {
 const { token } = res.data.data;
     if (res.status === 200||res.status===201) {
 
-      Cookies.set('token_admin', token, {
-        expires: 1,
-         path: '/',
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'Strict',
-      });
+      Cookies.set('token_admin', token, {expires: 1,});
 
       toast.success('تم تسجيل الدخول بنجاح 🎉');
      setTimeout(() => {
